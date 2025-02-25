b5b15964c9ccc904ad842dd9d099180694f8242d
siya-pandey-bluelupin <siya@bluelupin.com>
2025-02-25 10:46:49 +0530

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+// let score=Infinity
+// console.log(typeof(score))
+
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+// "33"=>33
+//"33abc"=>NaN
+//true=>1
+//false=>0
+//null=>0
+//undefined=>NaN
+//""=>0
+//""=>false                 
+//"hllo"=>true                 
+//{}=>NaN
+//[]=>0
+//NaN=>NaN
+//Infinity=>Infinity
+
+// let isLoggedIn="hi"
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log("booleanIsLoggedIn=",booleanIsLoggedIn)  
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(typeof(stringNumber))
+console.log("stringNumber=",stringNumber)
