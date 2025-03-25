3cc4d6eebae2df1ce864eea62b67ce65daefca51
siya-pandey-bluelupin <siya@bluelupin.com>
2025-03-25 10:10:54 +0530

    numbers and maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,38 @@
+// const score=400;
+// const balance=new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber=123.8966;
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds= 1000000000;
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-US'));
+
+//------------------------------------ MATHS -------------------------------------
+
+// console.log(Math);
+// console.log(Math.abs(-4.3));// converst a negative value to positive 
+// console.log(Math.round(4.993856985))//rounds the number off above .5 then the upper value is taken else lower value is taken
+// console.log(Math.ceil(4.1));//always rounds up
+// console.log(Math.floor(4.9));//always rounds down
+// console.log(Math.min(4,3,6,8,9,1,2,0));//returns the minimum value
+ 
+// console.log(Math.max(4,3,6,8,9,1,2,0));//returns the maximum value
+
+console.log(Math.floor(Math.random()*10)+1);//to get the rouded int lower limit
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max-min +1))+min)
+
+
+
+
+
+
+
